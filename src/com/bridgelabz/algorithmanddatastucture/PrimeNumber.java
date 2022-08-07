6b5e2cd6e939587bf703173a73b044c664171fa3
VighneshBilgi <vhbilgi@gmail.com>
2022-08-07 16:45:17 +0530

    Added: PrimeNumber

---
+++ src/com/bridgelabz/algorithmanddatastucture/PrimeNumber.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.algorithmanddatastucture;
+
+public class PrimeNumber {
+
+    public void printPrime(){
+        int i,count;
+
+        for(int j=2;j<=1000;j++)
+        {
+            count=0;
+            for(i=1;i<=j;i++)
+            {
+                if(j%i==0)
+                {
+                    count++;
+                }
+            }
+            if(count==2) {
+                System.out.print(j + " ");
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        PrimeNumber pn = new PrimeNumber();
+
+        System.out.println("Prime numbers from 1 to 1000 are :");
+        pn.printPrime();
+    }
+
+}
